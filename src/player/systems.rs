18710df3d27336ad9892d44e72587b0d14293ae0
Kvojps <josebazjunior09@gmail.com>
2026-04-03 13:10:57 -0300

    [refac]: move animate_player function to animation module and clean up systems.rs

--- src/player/systems.rs
+++ src/player/systems.rs
@@ -1,44 +1,6 @@
-use super::components::{AnimationSet, Player, PlayerAnimation, PlayerSpriteSheets};
-use bevy::prelude::*;
-
 mod animation;
 mod movement;
 mod setup;
+pub use animation::animate_player;
 pub use movement::movement_player;
 pub use setup::{setup_player, setup_player_scene};
-
-pub fn animate_player(
-    time: Res<Time>,
-    mut query: Query<(&mut PlayerAnimation, &PlayerSpriteSheets, &mut Sprite), With<Player>>,
-) {
-    let Ok((mut animation, sheets, mut sprite)) = query.single_mut() else {
-        return;
-    };
-
-    let frame_count = animation.active_frame_count();
-    if frame_count <= 1 {
-        return;
-    }
-
-    animation.timer.tick(time.delta());
-    if animation.timer.just_finished() {
-        animation.frame = (animation.frame + 1) % frame_count;
-        apply_animation_to_sprite(&animation, sheets, &mut sprite);
-    }
-}
-
-fn apply_animation_to_sprite(
-    animation: &PlayerAnimation,
-    sheets: &PlayerSpriteSheets,
-    sprite: &mut Sprite,
-) {
-    if let Some(texture_atlas) = sprite.texture_atlas.as_mut() {
-        let (image, layout) = match animation.set {
-            AnimationSet::Idle => (&sheets.idle_image, &sheets.idle_layout),
-            AnimationSet::Walk => (&sheets.walk_image, &sheets.walk_layout),
-        };
-        sprite.image = image.clone();
-        texture_atlas.layout = layout.clone();
-        texture_atlas.index = animation.atlas_index();
-    }
-}
--- src/player/systems/animation.rs
+++ src/player/systems/animation.rs
@@ -1,5 +1,25 @@
-use crate::player::components::{AnimationSet, PlayerAnimation, PlayerSpriteSheets};
+use crate::player::components::{AnimationSet, Player, PlayerAnimation, PlayerSpriteSheets};
 use bevy::prelude::*;
+
+pub fn animate_player(
+    time: Res<Time>,
+    mut query: Query<(&mut PlayerAnimation, &PlayerSpriteSheets, &mut Sprite), With<Player>>,
+) {
+    let Ok((mut animation, sheets, mut sprite)) = query.single_mut() else {
+        return;
+    };
+
+    let frame_count = animation.active_frame_count();
+    if frame_count <= 1 {
+        return;
+    }
+
+    animation.timer.tick(time.delta());
+    if animation.timer.just_finished() {
+        animation.frame = (animation.frame + 1) % frame_count;
+        apply_animation_to_sprite(&animation, sheets, &mut sprite);
+    }
+}
 
 pub fn apply_animation_to_sprite(
     animation: &PlayerAnimation,
