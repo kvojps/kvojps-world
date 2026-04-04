53b1f585e4dfcbaa9f9eab994d7aba5d800dafa8
Kvojps <josebazjunior09@gmail.com>
2026-04-03 21:40:50 -0300

    [refac]: improve spawn main tilemap logic

--- src/world/systems.rs
+++ src/world/systems.rs
@@ -1,18 +1,25 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+use core::panic;
 use serde_json::Value;
 use std::fs;
 
 const MAP_FILE_PATH: &str = "assets/world/map.tmj";
+const TILE_SIZE: f32 = 16.0;
+const TILE_SCALE: f32 = 2.0;
+const Z_LAYER: f32 = -1.0;
 
 pub fn setup_world_map(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let map = _load_map();
     let grass: Handle<Image> = asset_server.load("world/grass.png");
     let path: Handle<Image> = asset_server.load("world/path.png");
     let water: Handle<Image> = asset_server.load("world/water.png");
     let terrain_textures = vec![grass, path, water];
-    let map = _load_map();
+
+    _spawn_main_tilemap(&mut commands, &map, terrain_textures);
 }
 
-pub fn _load_map() -> Map {
+fn _load_map() -> Map {
     let Ok(content) = fs::read_to_string(&MAP_FILE_PATH) else {
         panic!("Failed to read map file at '{}'", MAP_FILE_PATH);
     };
@@ -71,5 +78,63 @@ fn _map_gid_to_tile(gid: u32) -> char {
         2 => 'P',
         3 => 'W',
         _ => panic!("Unknown tile GID: {}", gid),
+    }
+}
+
+fn _spawn_main_tilemap(commands: &mut Commands, map: &Map, textures: Vec<Handle<Image>>) {
+    let map_entity = commands.spawn_empty().id();
+    let map_size = TilemapSize {
+        x: map.width as u32,
+        y: map.height as u32,
+    };
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let tile = map.tiles[y][x];
+
+            let position = TilePos {
+                x: x as u32,
+                y: y as u32,
+            };
+
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position,
+                    texture_index: TileTextureIndex(_map_tile_to_gid(tile)),
+                    tilemap_id: TilemapId(map_entity),
+                    ..default()
+                })
+                .id();
+
+            tile_storage.set(&position, tile_entity);
+        }
+    }
+
+    commands.entity(map_entity).insert(TilemapBundle {
+        grid_size: TilemapGridSize {
+            x: TILE_SIZE,
+            y: TILE_SIZE,
+        },
+        anchor: TilemapAnchor::Center,
+        map_type: TilemapType::Square,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Vector(textures),
+        tile_size: TilemapTileSize {
+            x: TILE_SIZE,
+            y: TILE_SIZE,
+        },
+        transform: Transform::from_xyz(0.0, 0.0, Z_LAYER).with_scale(Vec3::splat(TILE_SCALE)),
+        ..default()
+    });
+}
+
+fn _map_tile_to_gid(tile: char) -> u32 {
+    match tile {
+        'G' => 0,
+        'P' => 1,
+        'W' => 2,
+        _ => panic!("Unknown tile type: {}", tile),
     }
 }
